4546cfa5bfb3cc614b0a364811f0309c8a6be501
William Zhang <william03zhang@gmail.com>
2024-07-22 13:08:38 -0400

    creation of login-activity branch and java file

--- B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/LoginActivity.java
+++ B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/LoginActivity.java
@@ -3,4 +3,5 @@ package com.example.b07demosummer2024;
 import android.app.Activity;
 
 public class LoginActivity extends Activity {
+    db = FirebaseDatabase.getInstance("https://b07-demo-summer-2024-default-rtdb.firebaseio.com/");
 }
