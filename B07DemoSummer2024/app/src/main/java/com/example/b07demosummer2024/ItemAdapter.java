a4b34c377857e712a8604fdc6eee5848f668fc47
m2zhang <marilyn2zhang@gmail.com>
2024-07-23 00:07:05 -0400

    feat: Added ItemAdapter

--- B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/ItemAdapter.java
+++ B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/ItemAdapter.java
@@ -1,4 +1,86 @@
 package com.example.b07demosummer2024;
 
-public class ItemAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.io.IOException;
+import java.net.URL;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+    private List<Item> itemList;
+
+    public ItemAdapter(List<Item> itemList) {
+        this.itemList = itemList;
+    }
+
+    @NonNull
+    @Override
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
+        Item item = itemList.get(position);
+        holder.textViewLotNumber.setText(item.getLotNumber());
+        holder.textViewName.setText(item.getName());
+        holder.textViewCategory.setText(item.getCategory());
+        holder.textViewPeriod.setText(item.getPeriod());
+        holder.textViewDescription.setText(item.getDescription());
+
+        // Load image
+        new Thread(() -> {
+            try {
+                InputStream inputStream = new URL(item.getPicture()).openStream();
+                final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                holder.itemView.post(() -> holder.imageViewPicture.setImageBitmap(bitmap));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        // Load video
+        holder.videoViewVideo.setVideoURI(Uri.parse(item.getVideo()));
+        holder.videoViewVideo.seekTo(1);
+        holder.videoViewVideo.setOnPreparedListener(mp -> {
+            mp.setLooping(true);
+            holder.videoViewVideo.start();
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewLotNumber, textViewName, textViewCategory, textViewPeriod, textViewDescription;
+        ImageView imageViewPicture;
+        VideoView videoViewVideo;
+        public ItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewLotNumber = itemView.findViewById(R.id.textViewLotNum);
+            textViewName = itemView.findViewById(R.id.textViewName);
+            textViewCategory = itemView.findViewById(R.id.textViewCategory);
+            textViewPeriod = itemView.findViewById(R.id.textViewPeriod);
+            imageViewPicture = itemView.findViewById(R.id.imageViewImage);
+            videoViewVideo = itemView.findViewById(R.id.videoViewVideo);
+            textViewDescription = itemView.findViewById(R.id.textViewDescription);
+        }
+    }
 }
