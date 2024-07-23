26ac56028aada242fa6c02454f2358f108bef26a
m2zhang <marilyn2zhang@gmail.com>
2024-07-23 00:19:26 -0400

    feat: Updated Recycler view + loading of data
    Everything builds fine, it's just the data isn't loading properly just yet. Will fix soon

--- B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/HomeFragment.java
+++ B07DemoSummer2024/app/src/main/java/com/example/b07demosummer2024/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.b07demosummer2024;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,7 +11,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private ItemAdapter itemAdapter;
+    private List<Item> itemList;
+
+    private FirebaseDatabase db;
+    private DatabaseReference itemsRef;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,8 +39,34 @@ public class HomeFragment extends Fragment {
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         // Initialize and set the adapter for RecyclerView here
+        itemList = new ArrayList<>();
+        itemAdapter = new ItemAdapter(itemList);
+        recyclerView.setAdapter(itemAdapter);
+
+        db = FirebaseDatabase.getInstance("https://b07-project-c1ef0-default-rtdb.firebaseio.com/");
+
 
         return view;
+    }
+
+    private void fetchItemsFromDatabase(String category) {
+        itemsRef = db.getReference("Items/" + category);
+        itemsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                itemList.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Item item = snapshot.getValue(Item.class);
+                    itemList.add(item);
+                }
+                itemAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle possible errors
+            }
+        });
     }
 
     private void loadFragment(Fragment fragment) {
